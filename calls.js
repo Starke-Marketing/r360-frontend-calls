9f0d706c28a9d5f0953a7e2baa974d6b9690e5af
Poncho@Starke <alfonso@wearestarke.com>
2021-08-11 13:46:10 -0500

    Updated to use memberid and email

--- calls.js
+++ calls.js
@@ -1,26 +1,23 @@
 const domain = window.location.hostname;
-const apiBaseURI = `${window.location.protocol}//r360.successionadvisors.com`;
+const apiBaseURI = `https://r360.successionadvisors.com`;
 const publicKey = "exlpGzluuIw4FgkMGFg8kR9ouACJgKvf";
 
-window.onload = (event) => {
+window.onload = () => {
     const loginButton = document.querySelector(".login-button");
     const signUpForm = document.querySelector(".signup-form");
 
     if (loginButton) {
-      const identifier = loginButton.getAttribute("data-identifier");
-      loginButton.addEventListener("click", (e) => autoLogin(identifier));
-    }
-
-    if (signUpForm) {
-      const identifier = signUpForm.getAttribute("data-identifier");
-      form.addEventListener("submit", (e) => autoSignUp(identifier));
+      const member_id = loginButton.getAttribute("data-memberid");
+      const email = loginButton.getAttribute("data-email");
+      loginButton.addEventListener("click", () => autoLogin(member_id, email));
     }
 };
 
-function autoLogin(identifier) {
+function autoLogin(member_id, email) {
   let formData = new FormData();
   formData.append("auth_key", publicKey);
-  formData.append("user_identifier", identifier);
+  formData.append("member_id", member_id);
+  formData.append("email", email);
 
   fetch(`${apiBaseURI}/api/endpoint/user/auth`, {
     method: "post",
@@ -34,23 +31,6 @@ function autoLogin(identifier) {
     .catch((err) => console.log(err));
 }
 
-function autoSignUp(identifier) {
-  let formData = new FormData();
-  formData.append("auth_key", publicKey);
-  formData.append("user_identifier", identifier);
-
-  fetch(`${apiBaseURI}/api/endpoint/user/create`, {
-    method: "post",
-    redirect: "follow",
-    body: formData,
-  })
-    .then((response) => response.json())
-    .then((data) =>
-      data.result ? goToSite(data.secure_key) : console.log("failed")
-    )
-    .catch((err) => console.log(err));
-}
-
 function goToSite(secureKey) {
-    window.open(`${apiBaseURI}?secure_key=${secureKey}`);
+  window.open(`${apiBaseURI}?secure_key=${secureKey}`);
 }
