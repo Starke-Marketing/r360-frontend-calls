208e668893ddcf97104cec74bf88d8a63623dee3
Poncho <e.alfonsopedroza58@gmail.com>
2021-11-15 17:19:50 -0600

    Fixed safari blocking pop ups issue

--- calls.js
+++ calls.js
@@ -1,17 +1,19 @@
 const apiBaseURI = `https://r360.successionadvisors.com`;
 const publicKey = "exlpGzluuIw4FgkMGFg8kR9ouACJgKvf";
 
-window.onload = () => {
-    const loginButton = document.getElementById("r360-login-button");
+const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
-    if (loginButton) {
-      const member_id = loginButton.getAttribute("data-memberid");
-      const email = loginButton.getAttribute("data-email");
-      loginButton.addEventListener("click", () => autoLogin(member_id, email));
-    }
+window.onload = () => {
+  const loginButton = document.getElementById("r360-login-button");
+
+  if (loginButton) {
+    const member_id = loginButton.getAttribute("data-memberid");
+    const email = loginButton.getAttribute("data-email");
+    loginButton.addEventListener("click", () => autoLogin(member_id, email));
+  }
 };
 
-function autoLogin(member_id, email) {
+const autoLogin = (member_id, email) => {
   let formData = new FormData();
   formData.append("auth_key", publicKey);
   formData.append("member_id", member_id);
@@ -29,6 +31,19 @@ function autoLogin(member_id, email) {
     .catch((err) => console.log(err));
 }
 
-function goToSite(secureKey) {
-  window.open(`${apiBaseURI}?secure_key=${secureKey}`);
+const goToSite = (secureKey) => {
+  const redirectUrl = getUrlString(secureKey);
+  isSafari ? redirectSafari(redirectUrl) : redirectAnyBrowser(redirectUrl);
+}
+
+const redirectAnyBrowser = (redirectUrl) => {
+  window.open(redirectUrl);
+};
+
+const redirectSafari = (redirectUrl) => {
+  window.location.assign(redirectUrl);
+};
+
+const getUrlString = (secureKey) => {
+  return `${apiBaseURI}?secure_key=${secureKey}`;
 }
